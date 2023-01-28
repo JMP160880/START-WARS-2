c5b8d8f24edf4fb0a6c12f61b22a10e7f1663500
JMP160880 <javiermoranpando@gmail.com>
2023-01-28 16:21:37 +0000

    arreglado de momento

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -10,12 +10,12 @@ const Card = (props) =>{
                 <h5 className="card-title text-start fw-bold">{props.nombre}</h5>
                 <p className="card-text text-start">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
                 <div className="d-flex justify-content-between">
-                    <a Link to="#" class="card-link">
-                        <button type="button" class="btn btn-outline-primary">Learn more!</button>
-                    </a>
-                    <a Link to="#" class="card-link">
-                        <button type="button" class="btn btn-outline-warning"><i class="fa fa-heart"></i></button>
-                    </a>
+                    <Link to="#" className="card-link">
+                        <button type="button" className="btn btn-outline-primary">Learn more!</button>
+                    </Link>
+                    <Link to="#" className="card-link">
+                        <button type="button" className="btn btn-outline-warning"><i className="fa fa-heart"></i></button>
+                    </Link>
                 </div>
                
             </div>

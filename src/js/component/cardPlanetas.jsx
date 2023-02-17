bf3b1ffb4748d135e98de429ce2c696c60094551
JMP160880 <javiermoranpando@gmail.com>
2023-02-17 18:26:25 +0000

    falta mejorar boton navbar

--- src/js/component/cardPlanetas.jsx
+++ src/js/component/cardPlanetas.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import {Context} from "../store/appContext.js"
 
 const CardPlanetas   = (props) =>{
+    const{store,actions}= useContext(Context)
+	const params = useParams();
+
     return(
     
-        <div className="card d-flex content-justify m-2 border-warning border border-3">
-            <img src={"https://starwars-visualguide.com/assets/img/planets/"+ props.idPlaneta +".jpg"} className="card-img-top"height="350px" alt="..."/>
+        <div className="col-md card d-flex content-justify m-2 border-warning border border-3">
+            <img src={"https://starwars-visualguide.com/assets/img/planets/"+ props.idPlaneta +".jpg"} className="card-img-top"height="250px" width="150px" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title text-start fw-bold">{props.nombrePlaneta}</h5>
                 <p className="card-text text-start"></p>
@@ -14,7 +19,8 @@ const CardPlanetas   = (props) =>{
                         <button type="button" className="btn btn-outline-primary border-primary border border-2 fw-bold">Learn more!</button>
                     </Link>
                     <Link to="#" className="card-link">
-                        <button type="button" className="btn btn-outline-warning"><i className="fa fa-heart"></i></button>
+                        <button onClick={() => actions.addFav(props.nombrePlaneta)} 
+                        type="button" className="btn btn-outline-warning"><i className="fa fa-heart"></i></button>
                     </Link>
                 </div>
                
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -10,7 +10,7 @@ export const Navbar = () => {
 	const params = useParams();
 
 	return (
-		<nav className="navbar navbar-light border-warning border border-3 m-2">
+		<nav className="col-md navbar navbar-light border-warning border border-3 m-2">
 			<Link to="/">
 			<img src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png?region=0,0,586,254" width="250px" className="img-thumbnail bg-dark ms-5 border-warning border border-3" alt="..."/>
 			</Link>
